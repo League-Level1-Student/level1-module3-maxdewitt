9827a24eadc86ef905ded98d388c0891099745af
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-04-24 16:55:22 -0700

    netflix in progress

---
+++ src/Netflix.java
@@ -0,0 +1,10 @@
+
+public class Netflix {
+public static void main(String[] args) {
+	Movie movie = new Movie("Movie", 1);
+	Movie movie1 = new Movie("Movie1", 12);
+	Movie movie2 = new Movie("Movie2", 13);
+	Movie movie3 = new Movie("Movie3", 14);
+	movie.getTicketPrice();
+}
+}
